99407a281de52a7b7bc3c488580443daa38de04f
Flavio Rodriguez <ihaio95@gmail.com>
2022-08-04 22:06:59 -0300

    se agrgaron rutas y login

--- server.js
+++ server.js
@@ -3,12 +3,22 @@ const path = require("path")
 const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 3000;
+const nunjucks = require("nunjucks")
+const routes = require("./routes/index")
 
 require("./db")()
 
 
-app.use(express.json)
+app.set("view engine", "njk")
+nunjucks.configure("views", {
+    autoescape: true,
+    express: app,
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
 app.use(express.static(path.join(__dirname, "public")))
+app.use(routes)
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`)
